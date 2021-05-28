7cd2283504a0f199488c08e1e6e611e7be0a2893
jbrit <pro.ajibolaojo@gmail.com>
2021-05-28 13:58:55 +0100

    queryset export

--- railinfo/views.py
+++ railinfo/views.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse
+from db import query_lines, query_stations
 
 def lines_view(request):
-    return JsonResponse({})
+    return JsonResponse(query_lines(), safe=False)
 
 
 def stations_view(request):
-    return JsonResponse({})
+    return JsonResponse(query_stations(), safe=False)
